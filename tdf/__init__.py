c1095b03b29712035025ec2c67567b21d87ffd4c
Nathan Woodbury <nathanswoodbury@gmail.com>
2014-08-08 11:54:25 -0600

    Automatic loading of js for client dependencies.

--- tdf/__init__.py
+++ tdf/__init__.py
@@ -6,4 +6,7 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_route('index', '/')
     config.scan('.app.views.index')
+
+    config.add_static_view('public', 'public')
+
     return config.make_wsgi_app()
--- tdf/app/views/index.py
+++ tdf/app/views/index.py
@@ -2,6 +2,7 @@ from pyramid.view import (
     view_config,
     view_defaults
 )
+import json
 
 
 @view_defaults(renderer='templates/default.pt')
@@ -9,6 +10,11 @@ class IndexView:
 
     def __init__(self, request):
         self.request = request
+
+    @property
+    def dependencies(self):
+        with open('clientdependencies.json', 'r') as f:
+            return json.load(f).items()
 
     @view_config(route_name='index')
     def index(self):
